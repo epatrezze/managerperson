bd1c5c80dc2bd37c6cc24a5b63248382d4403ecd
bannzp <evandro.patrezze@hotmail.com>
2021-07-13 18:15:49 -0300

    Initial commit

---
+++ src/main/java/com/digital/innovation/one/managerperson/ManagerpersonApplication.java
@@ -0,0 +1,13 @@
+package com.digital.innovation.one.managerperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerpersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerpersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/innovation/one/managerperson/ManagerpersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.innovation.one.managerperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerpersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
