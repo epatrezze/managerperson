6e73348bc2146ae6dde52b377f0e198091e1652b
bannzp <evandro.patrezze@hotmail.com>
2021-07-14 01:01:13 -0300

    Add class Person, Phone and enum PhoneType.

--- src/main/java/com/digital/innovation/one/managerperson/ManagerpersonApplication.java
+++ src/main/java/com/digital/innovation/one/managerperson/ManagerpersonApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ManagerpersonApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ManagerpersonApplication.class, args);
 	}
 
