8836cd252bdf1225550d9560ef7c5ac4b3f7df3f
bannzp <evandro.patrezze@hotmail.com>
2021-07-19 15:55:28 -0300

    Add README.md file.

--- src/main/java/com/digital/innovation/one/managerperson/service/PersonService.java
+++ src/main/java/com/digital/innovation/one/managerperson/service/PersonService.java
@@ -6,6 +6,7 @@ import com.digital.innovation.one.managerperson.entity.Person;
 import com.digital.innovation.one.managerperson.exception.PersonNotFoundException;
 import com.digital.innovation.one.managerperson.mapper.PersonMapper;
 import com.digital.innovation.one.managerperson.repository.PersonRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,16 +15,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
 
     private PersonRepository personRepository;
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     public MessageResponseDTO createPerson(@RequestBody PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
